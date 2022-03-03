c16c2ffbb93e4019ca6a1fa18e1cccc641339789
anagramiccoder <jdvidad31@gmail.com>
2022-03-03 21:53:50 +0800

    navmenu

--- portalsite/portalsite/urls.py
+++ portalsite/portalsite/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from voters.views import home_view
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',home_view,name='home')
 ]
