791f4287132e5fe83ba082aa3d555b36fba07c04
anagramiccoder <jdvidad31@gmail.com>
2022-03-30 22:24:58 +0800

    Added faci and rep model

--- portalsite/voters/models.py
+++ portalsite/voters/models.py
@@ -5,6 +5,8 @@ from django.core.validators import RegexValidator
 class User(AbstractUser):
     is_admin=models.BooleanField('is admin',default=False)
     is_voter=models.BooleanField('is voter',default=False)
+    is_faci=models.BooleanField('is facilitator',default=False)
+    is_rep=models.BooleanField('is representative',default=False)
 class Admin(models.Model):
     user=models.OneToOneField(User,on_delete=models.CASCADE,primary_key=True)
 class Voter(models.Model):
@@ -15,6 +17,16 @@ class Voter(models.Model):
     Add=models.CharField(max_length=300)
     has_voted=models.BooleanField('Voted',default=False)
     contact=models.CharField(max_length=11, default='09000000000', validators=[RegexValidator(r'^[0-9]{11}$')])
+class Rep(models.Model):
+    user=models.OneToOneField(User,on_delete=models.CASCADE,primary_key=True)
+    mName=models.CharField(max_length=100)
+    vId=models.CharField(max_length=100)
+    pNum=models.CharField(max_length=100)
+    Add=models.CharField(max_length=300)
+    has_voted=models.BooleanField('Voted',default=False)
+    contact=models.CharField(max_length=11, default='09000000000', validators=[RegexValidator(r'^[0-9]{11}$')])
+class Faci(models.Model):
+    user=models.OneToOneField(User,on_delete=models.CASCADE,primary_key=True)
 class Precinct(models.Model):
     pNum=models.CharField(max_length=100,unique=True)
     pAdd=models.CharField(max_length=300)
