df950323e1c749da329758a3b98384d43816f7f9
anagramiccoder <jdvidad31@gmail.com>
2022-04-01 01:21:58 +0800

    fixed Representative model

--- portalsite/voters/admin.py
+++ portalsite/voters/admin.py
@@ -1,9 +1,10 @@
+from http.client import ResponseNotReady
 from django.contrib import admin
-from .models import User, Admin, Voter, Precinct, Rep, Faci
+from .models import User, Admin, Voter, Precinct, Faci,Repre
 # Register your models here.
 admin.site.register(User)
 admin.site.register(Admin)
 admin.site.register(Voter)
 admin.site.register(Precinct)
-admin.site.register(Rep)
 admin.site.register(Faci)
+admin.site.register(Repre)
--- portalsite/voters/models.py
+++ portalsite/voters/models.py
@@ -17,16 +17,19 @@ class Voter(models.Model):
     Add=models.CharField(max_length=300)
     has_voted=models.BooleanField('Voted',default=False)
     contact=models.CharField(max_length=11, default='09000000000', validators=[RegexValidator(r'^[0-9]{11}$')])
-class Rep(models.Model):
+class Faci(models.Model):
     user=models.OneToOneField(User,on_delete=models.CASCADE,primary_key=True)
+class Precinct(models.Model):
+    pNum=models.CharField(max_length=100,unique=True)
+    pAdd=models.CharField(max_length=300)
+class Repre(models.Model):
+    user=models.ForeignKey(User,on_delete=models.CASCADE)
+    id=models.AutoField(primary_key=True)
+    vFname=models.CharField(max_length=100,default="")
+    vLname=models.CharField(max_length=100,default="")
     mName=models.CharField(max_length=100)
     vId=models.CharField(max_length=100)
     pNum=models.CharField(max_length=100)
     Add=models.CharField(max_length=300)
     has_voted=models.BooleanField('Voted',default=False)
     contact=models.CharField(max_length=11, default='09000000000', validators=[RegexValidator(r'^[0-9]{11}$')])
-class Faci(models.Model):
-    user=models.OneToOneField(User,on_delete=models.CASCADE,primary_key=True)
-class Precinct(models.Model):
-    pNum=models.CharField(max_length=100,unique=True)
-    pAdd=models.CharField(max_length=300)
