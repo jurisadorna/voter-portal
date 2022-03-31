4c8a5430ba3bb208fc01bb737c66bf9ee4ffbf40
anagramiccoder <jdvidad31@gmail.com>
2022-03-31 22:41:49 +0800

    fixed Rep and Faci DB

--- portalsite/voters/admin.py
+++ portalsite/voters/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from .models import User, Admin, Voter, Precinct
+from .models import User, Admin, Voter, Precinct, Rep, Faci
 # Register your models here.
 admin.site.register(User)
 admin.site.register(Admin)
 admin.site.register(Voter)
 admin.site.register(Precinct)
+admin.site.register(Rep)
+admin.site.register(Faci)
